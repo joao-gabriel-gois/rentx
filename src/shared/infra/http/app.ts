e18b4245b6505c37ee88155469b5e293068627ad
Joao Gabriel <gois.jg.dev@gmail.com>
2021-09-05 01:56:14 -0300

    Removing Sentry debug route

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -56,8 +56,4 @@ app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorHandler);
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-
 export default app;
